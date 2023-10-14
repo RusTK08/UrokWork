2d15f8ae9f175d619787fcec84126dce3c49966d
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-14 18:44:27 +0300

    Ur3

---
+++ Ur2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
